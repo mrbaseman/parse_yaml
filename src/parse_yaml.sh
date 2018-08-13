b6dd21c37d15b4a2fb252c87f8c7236d37241432
Martin Hecht <mrbaseman@gmx.de>
2018-08-13 16:59:44 +0200

    support different depths of indentation
    in order to parse a file that uses 4 spaces of indentation, just
    `export i="    "` # four space characters.
    i is simply the string used to indent one more level

--- src/parse_yaml.sh
+++ src/parse_yaml.sh
@@ -3,7 +3,7 @@
 function parse_yaml {
    local prefix=$2
    local separator=${3:-_}
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=${fs:-$(echo @|tr @ '\034')}
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=${fs:-$(echo @|tr @ '\034')} i=${i:-  }
    cat $1 | \
    awk -F$fs "{multi=0; 
        if(match(\$0,/$s\|$s\$/)){multi=1; sub(/$s\|$s\$/,\"\");}
@@ -11,12 +11,12 @@ function parse_yaml {
        while(multi>0){
            str=\$0; gsub(/^$s/,\"\", str);
 	   indent=index(\$0,str);
-	   indentstr=substr(\$0, 0, indent-1) \"  \";
+	   indentstr=substr(\$0, 0, indent-1) \"$i\";
 	   obuf=\$0;
 	   getline;
 	   while(index(\$0,indentstr)){
 	       obuf=obuf substr(\$0, length(indentstr)+1);
-	       if (multi==1){obuf=obuf  \"\\\\n\";}
+	       if (multi==1){obuf=obuf \"\\\\n\";}
 	       if (multi==2){
 	           if(match(\$0,/^$s\$/))
 		       obuf=obuf \"\\\\n\";
@@ -26,35 +26,35 @@ function parse_yaml {
 	   }
 	   sub(/$s\$/,\"\",obuf);
 	   print obuf;
-	   multi=0; 
+	   multi=0;
            if(match(\$0,/$s\|$s\$/)){multi=1; sub(/$s\|$s\$/,\"\");}
-           if(match(\$0,/$s>$s\$/)){multi=2; sub(/$s>$s\$/,\"\");}	   
+           if(match(\$0,/$s>$s\$/)){multi=2; sub(/$s>$s\$/,\"\");}
        }
    print}" | \
    sed -ne "s|,$s\]$s\$|]|" \
-        -e ":1;s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s,$s\(.*\)$s\]|\1\2: [\3]\n\1  - \4|;t1" \
-        -e "s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s\]|\1\2:\n\1  - \3|;" \
-        -e ":2;s|^\($s\)-$s\[$s\(.*\)$s,$s\(.*\)$s\]|\1- [\2]\n\1  - \3|;t2" \
-        -e "s|^\($s\)-$s\[$s\(.*\)$s\]|\1-\n\1  - \2|;p" | \
+        -e ":1;s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s,$s\(.*\)$s\]|\1\2: [\3]\n\1$i- \4|;t1" \
+        -e "s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s\]|\1\2:\n\1$i- \3|;" \
+        -e ":2;s|^\($s\)-$s\[$s\(.*\)$s,$s\(.*\)$s\]|\1- [\2]\n\1$i- \3|;t2" \
+        -e "s|^\($s\)-$s\[$s\(.*\)$s\]|\1-\n\1$i- \2|;p" | \
    sed -ne "s|,$s}$s\$|}|" \
-        -e ":1;s|^\($s\)-$s{$s\(.*\)$s,$s\($w\)$s:$s\(.*\)$s}|\1- {\2}\n\1  \3: \4|;t1" \
-        -e "s|^\($s\)-$s{$s\(.*\)$s}|\1-\n\1  \2|;" \
-        -e ":2;s|^\($s\)\($w\)$s:$s{$s\(.*\)$s,$s\($w\)$s:$s\(.*\)$s}|\1\2: {\3}\n\1  \4: \5|;t2" \
-        -e "s|^\($s\)\($w\)$s:$s{$s\(.*\)$s}|\1\2:\n\1  \3|;p" | \
+        -e ":1;s|^\($s\)-$s{$s\(.*\)$s,$s\($w\)$s:$s\(.*\)$s}|\1- {\2}\n\1$i\3: \4|;t1" \
+        -e "s|^\($s\)-$s{$s\(.*\)$s}|\1-\n\1$i\2|;" \
+        -e ":2;s|^\($s\)\($w\)$s:$s{$s\(.*\)$s,$s\($w\)$s:$s\(.*\)$s}|\1\2: {\3}\n\1$i\4: \5|;t2" \
+        -e "s|^\($s\)\($w\)$s:$s{$s\(.*\)$s}|\1\2:\n\1$i\3|;p" | \
    sed -ne "s|^\($s\):|\1|" \
         -e "s|^\($s\)-$s[\"']\(.*\)[\"']$s\$|\1$fs$fs\2|p;t" \
         -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p;t" \
         -e "s|^\($s\)-$s\(.*\)$s\$|\1$fs$fs\2|" \
         -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|" \
         -e "s|$s\$||p" | \
-   awk -F$fs '{
-      indent = length($1)/2;
-      vname[indent] = $2;
+   awk -F$fs "{
+      indent = length(\$1)/length(\"$i\");
+      vname[indent] = \$2;
       for (i in vname) {if (i > indent) {delete vname[i]; idx[i]=0}}
-      if(length($2)== 0){  vname[indent]= ++idx[indent] };
-      if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) { vn=(vn)(vname[i])("'$separator'")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, vname[indent], $3);
+      if(length(\$2)== 0){  vname[indent]= ++idx[indent] };
+      if (length(\$3) > 0) {
+         vn=\"\"; for (i=0; i<indent; i++) { vn=(vn)(vname[i])(\"$separator\")}
+         printf(\"%s%s%s=\\\"%s\\\"\n\", \"$prefix\",vn, vname[indent], \$3);
       }
-   }'
+   }"
 }
